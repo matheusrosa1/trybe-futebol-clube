13ad4449fd6f5cb8bac81d57e77afa350fc2f858
Matheus Rosa <matheusrosataxa@gmail.com>
2024-04-11 18:43:02 -0300

    fix: validateLogin (invalid password)

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -11,12 +11,12 @@ class Validations {
 
     const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (!emailRegex.test(email)) {
-      return res.status(401).json({ message: 'Invalid email' });
-    }
-    if (password.length < 6) {
+    if (!emailRegex.test(email) || password.length < 6) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
+    /*     if (password.length < 6) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    } */
 
     return next();
   }

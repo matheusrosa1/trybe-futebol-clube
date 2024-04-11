8d0c434587d2586c33834afc095333ac9e557052
Matheus Rosa <matheusrosataxa@gmail.com>
2024-04-11 00:27:42 -0300

    construindo migration User

---
+++ app/backend/src/Interfaces/users/IUser.ts
@@ -0,0 +1,12 @@
+import { Identifiable } from '..';
+
+export interface ILogin {
+  email: string;
+  password: string;
+}
+
+export interface IUser extends Identifiable, ILogin {
+  username: string;
+}
+
+export type IUserResponse = Omit<IUser, 'password'>;
---
+++ app/backend/src/database/migrations/02-create-users-table.ts
@@ -0,0 +1,33 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import { username } from '../config/database';
+import { IUser } from '../../Interfaces/users/IUser'
+
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IUser>>('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    });
+  },
+
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('users');
+  },
+};

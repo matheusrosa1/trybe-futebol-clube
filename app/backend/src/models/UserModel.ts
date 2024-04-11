2d3260f9618a0983bc02e36c0b2bb383136a6ef7
Matheus Rosa <matheusrosataxa@gmail.com>
2024-04-11 15:48:46 -0300

    feat: UserModel

---
+++ app/backend/src/models/UserModel.ts
@@ -0,0 +1,25 @@
+import SequelizeUser from '../database/models/SequelizeUser';
+import { IUser } from '../Interfaces/users/IUser';
+import { IUserModel } from '../Interfaces/users/IUserModel';
+
+export default class UserModel implements IUserModel {
+  private model = SequelizeUser;
+
+  async findAll(): Promise<IUser[]> {
+    const dbData = await this.model.findAll();
+    return dbData.map(({ id, username, role, email, password }) => (
+      { id, username, role, email, password }
+    ));
+  }
+
+  async findById(id: number): Promise<IUser | null> {
+    const dbData = await this.model.findByPk(id);
+    if (dbData == null) return null;
+    const { username, role, email, password }: IUser = dbData;
+    return { id, username, role, email, password };
+  }
+
+/*   async findByEmail(email: string): Promise<IUser | null> {
+    const dbData = await this.model.findOne(email);
+  } */
+}

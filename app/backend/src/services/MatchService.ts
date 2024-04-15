daa94beb8fce959721bf06f737c9b68c3c2314cd
Matheus Rosa <matheusrosataxa@gmail.com>
2024-04-15 19:38:10 -0300

    testing: matchess/:id/finish

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -18,19 +18,14 @@ export default class MatchService {
     return { status: 'SUCCESSFUL', data: matchesByQuery };
   }
 
-  public async finishingMatch(
-    id: number,
-  ): Promise<ServiceResponse<ServiceMessage>> {
+  public async finishingMatch(id: number): Promise<ServiceResponse<ServiceMessage>> {
     const findMatch = await this.matchModel.findById(id);
-    /*     if (findMatch?.inProgress === false) {
-      return { status: 'CONFLICT',
-        data: { message: 'Não é possível finalizar uma partida já finalizada' } };
-    } */
+
     if (!findMatch) {
       return { status: 'NOT_FOUND', data: { message: `Match ${id} not found` } };
     }
-    const turnFinished = findMatch.inProgress === false;
 
+    const turnFinished = findMatch.inProgress === false;
     await this.matchModel.update(id, { inProgress: turnFinished });
     return { status: 'SUCCESSFUL', data: { message: 'Finished' } };
   }
